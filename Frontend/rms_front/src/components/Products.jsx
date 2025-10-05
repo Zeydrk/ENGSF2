faf4144a5e2dffa447b9663412610cec60562184
1Rai1 <soronioclayton42@gmail.com>
2025-10-05 22:14:00 +0800

    extra kasi special ako

--- Frontend/rms_front/src/components/Products.jsx
+++ Frontend/rms_front/src/components/Products.jsx
@@ -1,11 +1,52 @@
 import {useState} from 'react';
-import {useProducts} from '../hooks/Products/useStudent';
+import {useProducts} from '../hooks/Products/useProduct';
 
 export default function  Products(){
-    const [showForm, setShowFrom] = useState(false);
+    const [showForm, setShowForm] = useState(false);
+    const products = useProducts();
+
+    const handleForm = () => {
+        setShowForm(true);
+    }
 
     function FormComponent(){
         const products = useProducts();
+        async function HandleSubmit(e){
+            e.preventDefault();
+
+            const data = new FormData(e.target);
+            const product ={
+                product_Name: data.get('p_Name'),
+                product_Price: data.get('p_Price')
+            };
+            await products.createProducts(product);
+        }
+        return (
+      <div className='createStudent'>
+        <form onSubmit={HandleSubmit}>{/*this da event*/}
+          <div className='mt-6'>
+            <label>
+              Product Name: 
+            <input type='text' name='p_Name' placeholder='Product Name'   />{/** Controlled components pamore */}
+            </label>
+          </div>
+          <div className='mt-6'>
+            <label>
+              Product Price: 
+            <input type='text' name='p_Price' placeholder='Product Price'   />
+            </label>
+          </div>
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+      );
     }
+    
+  return (
+    <div className='createProduct'>
+        <button  onClick={handleForm}>Add Product </button>
+        {showForm && <FormComponent/>}
+    </div>
+  )
 }
 
---
+++ Frontend/rms_front/src/hooks/Products/useProduct.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+export function useProduct(){
+    const [products, setProducts] = useProduct([]);
+    const BASE_URL = 'https://localhost:3000';
+
+    async function getAllProducts(){
+        const products = await axios.get(`${BASE_URL}/products`);
+        setProducts(products.data)
+    }
+    async function createProducts(){
+        const response = await axios.post(`${BASE_URL}/products/create`, {
+            product_Name:response.p_Name,
+            product_Price:response.p_Price
+        });
+        setProducts(prev => [...prev ,response.data])
+    }
+return {
+    products,
+    getAllProducts,
+    createProducts
+};
+}
