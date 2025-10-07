06a15d05010005973de0feb8edad662e8e80f24f
Zeydrk <zedricveran7@gmail.com>
2025-10-07 21:23:53 +0800

    Done with major functionalities in register, update when learned middlewares

--- Backend/src/admin/admins-controller.js
+++ Backend/src/admin/admins-controller.js
@@ -4,13 +4,13 @@ const models = require("../../models")
 // Functions for CRUD
 
 
-// async function createAdmin(req, res) {
-//     const admins = await models['Admin'].create({
-//         username: req.body.username,
-//         password: req.body.password
-//     })
-//     res.send(admins)
-// }
+async function createAdmin(req, res) {
+    const admins = await models['Admin'].create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    res.send(admins)
+}
 
 
 async function getAdmins(req, res) {
@@ -19,15 +19,10 @@ async function getAdmins(req, res) {
     res.send(response); // Unauthorized if no match found
 }
 
-// async function getOneAdmin(req, res) {
-//     const {username, password} = req.body 
-//     const response = await models['Admin'].findOne({where: {username, password}})
-//     res.send(response)
-// }
 
 // Exporting
 module.exports = {
-    // createAdmin,
+    createAdmin,
     getAdmins,
     // getOneAdmin
 }
