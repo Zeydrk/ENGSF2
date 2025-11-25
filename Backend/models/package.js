7237d55fb4979b16c123e6e80f0cfc0a31338788
erkiyel <20213699@s.ubaguio.edu>
2025-11-25 18:27:24 +0800

    draft package

--- Backend/models/package.js
+++ Backend/models/package.js
@@ -1,35 +1,38 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Package extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       Package.belongsTo(models.Seller, {
         foreignKey: 'seller_Id',
       });
     }
   }
+
   Package.init({
     seller_Id: {
       type: DataTypes.INTEGER,
+      allowNull: false,
       references: {
-        model: "Seller", 
+        model: "Sellers",
         key: "id"
-      }},
-    package_Name: DataTypes.STRING,
-    recipient_Name: DataTypes.STRING,
-    descrtion: DataTypes.STRING
+      }
+    },
+    description: DataTypes.STRING,
+    customer_Name: DataTypes.STRING,
+    package_Size: DataTypes.STRING,
+    price: DataTypes.DECIMAL(10,2),
+    handling_Fee: DataTypes.DECIMAL(10,2),
+    payment_Status: DataTypes.STRING,       // paid / unpaid
+    payment_Method: DataTypes.STRING,       // cash / gcash
+    package_Status: DataTypes.STRING        // pending / claimed
   }, {
     sequelize,
     modelName: 'Package',
-    updatedAt:false,
-    createdAt:false
+    updatedAt: false,
+    createdAt: false
   });
+
   return Package;
 };
